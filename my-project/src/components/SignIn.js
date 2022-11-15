42e89bea540ec93211c393e7c274f7393af7d234
Klirk <chernokulskiy200@gmail.com>
2022-11-15 18:46:57 +0200

    1.6.1

--- my-project/src/components/SignIn.js
+++ my-project/src/components/SignIn.js
@@ -35,7 +35,7 @@ export default function SignIn() {
               alt="Your Company"
             />
             <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-              Sign in to your account
+                Log in to your account
             </h2>
           </div>
           <form className="mt-8 space-y-6" action="#" method="POST">
